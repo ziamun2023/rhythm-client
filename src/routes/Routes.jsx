c49376faf703f1a8cbe9cd4960d05e85032cb6b2
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-06-07 23:49:00 +0600

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import Main from "../Main/Main";
+import Home from "../pages/Home/Home";
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path:'/',
+            element: <Home/>
+        }
+      ]
+    },
+  ]);
